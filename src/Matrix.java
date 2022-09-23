abe3bc9346c1c1e29a6ea6d10d218ea9cd16dc24
AyoubIMAMI <youbima17@gmail.com>
2022-09-23 12:01:50 +0200

    Matrix creation

---
+++ src/Matrix.java
@@ -0,0 +1,17 @@
+/**
+ * @author LE BIHAN Léo
+ * @author IMAMI Ayoub
+ *
+ * Matrix class : matrix where the crowd will move
+ */
+
+public class Matrix {
+        int m;
+        int n;
+
+        Matrix(int m, int n) {
+            this.m = m;
+            this.n = n;
+        }
+
+}
