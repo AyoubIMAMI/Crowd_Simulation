0cc73248fe6622b7582d9d96bd4fca1ae1033d4c
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-10-07 10:57:28 +0200

    now moving 1 case

--- src/Main.java
+++ src/Main.java
@@ -17,18 +17,20 @@ public class Main {
         int height = 3;
         Display display = new Display(lenght,height);
         MatrixEntity matrixEntity = new MatrixEntity(lenght,height,3);
+
         for(int i = 0 ; i < 3 ; i++){
             Position currentPos = Position.getRandomPosition(0,2);
             Position finalPos = Position.getRandomPosition(0,2);
             Entity ent = new Entity(currentPos, finalPos);
             matrixEntity.addEntity(ent);
         }
+
         display.displayGrid(matrixEntity);
         ArrayList<Entity> listEnt = matrixEntity.getListEntity();
         for(Entity entity: listEnt){
             entity.move(Position.getRandomLogicalPosition(entity.getCurrentPosition(), lenght, height));
+            display.updateGrid(matrixEntity.getListEntity());
+            Thread.sleep(750);
         }
-        Thread.sleep(2000);
-        display.updateGridV2(matrixEntity.getListEntity());
     }
 }
